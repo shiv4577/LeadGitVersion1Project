9cf7b7eef4c49e6e54393b494cb9929d5aa8a500
ASUS <ASUS@192.168.1.100>
2020-03-10 12:20:46 +0530

    this is my 1st commit

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+
+public class Test {
+public static void main(String[] args) {
+	
+	String Hungry="yes";
+	
+	if (Hungry=="yes") {
+		System.out.println("eat something");
+	}
+	else {
+		System.out.println("Do work");
+	}
+}
+}
